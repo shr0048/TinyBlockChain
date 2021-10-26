aa5161ba59e51ae4b8b244c8d916c627a58b4486
heradSim <shr0048@protonmail.com>
2021-10-27 05:54:08 +0900

    Add other operator

--- maths/FiniteField.py
+++ maths/FiniteField.py
@@ -17,3 +17,34 @@ class FieldElement:
 
     def __ne__(self, other):
         return not (self == other)
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Can't add two numbers in different fields")
+
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Can't add two numbers in different fields")
+
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __mul__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Can't add two numbers in different fields")
+
+        num = (self.num * other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __pow__(self, exponent):
+        n = exponent % (self.prime - 1)
+        num = pow(self.num, n) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Can't add two numbers in different fields")
+        return self * (other**(other.prime - 2))
--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 from maths import FiniteField
 
 if __name__ == '__main__':
-    a = FiniteField.FieldElement(3, 7)
-    b = FiniteField.FieldElement(4, 7)
+    f_1 = FiniteField.FieldElement(5, 7)
+    f_2 = FiniteField.FieldElement(3, 7)
 
-    print(a != b)
-    print(a == b)
+    print(f_1 / f_2)
