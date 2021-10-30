aac15e87b1424b03eb5388f9b415b46c3f5ba9d1
heradSim <shr0048@protonmail.com>
2021-10-31 02:22:06 +0900

    Add Curve class

---
+++ elliptic_curve/curve.py
@@ -0,0 +1,54 @@
+class Point:
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+
+        # That is infinite case, (identity of addition)
+        if self.x is None and self.y is None:
+            return
+        if self.y ** 2 != self.x ** 3 + a * x + b:
+            raise ValueError(f'{x}, {y} is not on the curve')
+
+    def __eq__(self, other):
+        return self.x == other.x and \
+               self.y == other.y and \
+               self.a == other.a and \
+               self.b == other.b
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            return TypeError(f'{self}, {other} are not same curve')
+
+        # if either operator is the identity of addition
+        if self.x is None and self.y is None:
+            return other
+        if other.x is None and other.y is None:
+            return self
+
+        # other is '-self'
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+
+        # if add same point
+        if self == other:
+            # when 'y=0' then return identity of addition
+            if self.y == 0:
+                return self.__class__(None, None, self.a, self.b)
+
+            else:
+                s = (3 * self.x ** 2 + self.a) / (2 * self.y)
+                new_x = (s ** 2) - (2 * self.x)
+                new_y = s * (self.x - new_x) - self.y
+                return self.__class__(new_x, new_y, self.a, self.b)
+
+        # Normal case
+        s = (other.y - self.y) / (other.x - self.x)
+        new_x = s ** 2 - self.x - other.x
+        new_y = s * (self.x - new_x) - self.y
+
+        return self.__class__(new_x, new_y, self.a, self.b)
--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 from maths import FiniteField
+from elliptic_curve import curve
 
 if __name__ == '__main__':
     f_1 = FiniteField.FieldElement(5, 7)
     f_2 = FiniteField.FieldElement(3, 7)
 
     print(f_1 / f_2)
+
+    curve_1 = curve.Point(-1, -1, 5, 7)
+    curve_2 = curve.Point(-1, -1, 5, 7)
+    res = curve_1 + curve_2
+
+    print(res.x, res.y)
