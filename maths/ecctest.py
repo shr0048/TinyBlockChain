f24974d453e8d24ac9923f690251aa2d6d18b442
shr0048 <shr0048_dev@protonmail.com>
2021-11-02 03:03:28 +0900

    Add curve add test

--- maths/ecctest.py
+++ maths/ecctest.py
@@ -24,3 +24,41 @@ class ECCTest(TestCase):
 
             with self.assertRaises(ValueError):
                 curve.Point(x, y, a, b)
+
+    def test_add(self):
+        prime = 223
+        a = FiniteField.FieldElement(0, prime)
+        b = FiniteField.FieldElement(7, prime)
+
+        add_points = [((170, 142), (60, 139)),
+                      ((47, 71), (17, 56)),
+                      ((143, 98), (76, 66))]
+
+        add_res = [curve.Point(FiniteField.FieldElement(220, prime),
+                               FiniteField.FieldElement(181, prime),
+                               a, b),
+                   curve.Point(FiniteField.FieldElement(215, prime),
+                               FiniteField.FieldElement(68, prime),
+                               a, b),
+                   curve.Point(FiniteField.FieldElement(47, prime),
+                               FiniteField.FieldElement(71, prime),
+                               a, b)
+                   ]
+
+        points = []
+        for coordinate_1, coordinate_2 in add_points:
+            points.append(
+                (
+                    curve.Point(FiniteField.FieldElement(coordinate_1[0], prime),
+                                FiniteField.FieldElement(coordinate_1[1], prime),
+                                a, b
+                                ),
+                    curve.Point(FiniteField.FieldElement(coordinate_2[0], prime),
+                                FiniteField.FieldElement(coordinate_2[1], prime),
+                                a, b
+                                )
+                )
+            )
+
+        for idx, point in enumerate(points):
+            self.assertEqual(point[0] + point[1], add_res[idx])
--- test.py
+++ test.py
@@ -1,4 +1,38 @@
-from maths import FiniteField, curve, ecctest
+from maths import FiniteField, curve
 
 if __name__ == '__main__':
-    pass
+    prime = 223
+    a = FiniteField.FieldElement(0, prime)
+    b = FiniteField.FieldElement(7, prime)
+
+    add_points = [((170, 142), (60, 139)),
+                  ((47, 71), (17, 56)),
+                  ((143, 98), (76, 66))]
+
+    points = []
+    for coordinate_1, coordinate_2 in add_points:
+        try:
+            points.append(
+                (
+                    curve.Point(FiniteField.FieldElement(coordinate_1[0], prime),
+                            FiniteField.FieldElement(coordinate_1[1], prime),
+                            a, b
+                            ),
+                    curve.Point(FiniteField.FieldElement(coordinate_2[0], prime),
+                             FiniteField.FieldElement(coordinate_2[1], prime),
+                             a, b
+                             )
+                )
+            )
+
+        except ValueError:
+            print("Error")
+            continue
+
+    for point_1, point_2 in points:
+        res = point_1 + point_2
+        print(point_1.a, point_1.b, point_1.x, point_1.y)
+        print(point_2.a, point_2.b, point_2.x, point_2.y)
+        print(f"Add: {res.a, res.b, res.x, res.y}")
+        print("\n")
+
